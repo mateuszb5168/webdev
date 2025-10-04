35d2daebe25881d7caa6de2ee6efa58c655c2323
mateuszb5168 <mateusz_biela@wp.pl>
2025-10-04 20:42:43 +0200

    finished 1st and 2sc chapters, nothing much, just learning fundamentals of js

---
+++ javaScript/JS1/quickStart_1.js
@@ -0,0 +1,13 @@
+// nothing really. the first chapter was in the browser console but to have some documentation im just gonna write the stuff here and explain it
+
+let camelCase; //initializes a variable, using camel case naming convention
+camelCase = 5; //sets the variable to 5 integer, ";" sign to end line
+camelCase === 5; //output True, specific comparison
+camelCase == "5";// clearly not, but welcome you crazy JavaScript, less specific comparison
+console.log("say something") //Where's the hello world? :(
+camelCase = camelCase - 3;// instead, camelCase -= 3;
+
+"5" - 3;// outputs 2, - operator converts string to a integer
+"5" + 3; //outputs 53, + is a valid operator for strings, therefore it converts the integer into a string
+
+//ok thats it for the first chapter, in the next one the guy will write !=browserConsole so yeah um a bit nicer
---
+++ javaScript/JS2/node.js
@@ -0,0 +1,2 @@
+alert("Hello JS");
+//i could have done console.log but lets jumpscare the user everytime the page is loaded
