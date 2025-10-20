3c6ac2d095c833c7d61d37e2882f60848854add6
mateuszb5168 <mateusz_biela@wp.pl>
2025-10-20 17:09:41 +0200

    mm done soem chapters nothing much kinda paperwork
    - but i knew this! ~ me later

---
+++ javaScript/JS5_MathMethods/main.js
@@ -0,0 +1,9 @@
+Math.trunc()//float to int
+Math.round()//float to int but round to the nearest integer
+Math.ceil()//float to int round up, floor does .reverse
+Math.pow()// to the power..
+Math.min()// selects the smallest value, kinda lazy but probably optimized, same with .max
+
+Math.random()// gives a value from 0 to 1, (a float value)
+Math.floor(Math.random() * x) + 1; //change x to the max number, it will be the max output from the .random function
+//nothing much
---
+++ javaScript/JS6_CodeChallenge/main.js
@@ -0,0 +1,11 @@
+//write code that returns a random number from your name
+
+let nameVar = ["m","a","t","h","e","w"];
+let IndexName = Math.floor(Math.random() * nameVar.length);
+let RandomLetter = nameVar[IndexName];
+console.log(RandomLetter);
+//so ez(100 times easier than the average easy leetcode problem)
+// - loser you hardcoded the list !! 
+// - shut up i dont even know how to pull in user inputs 
+// - then you didnt make it a function!!
+// - am i writing a whole app dumbass
