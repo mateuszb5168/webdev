b6082c26dedb04387cecde393e474a42cedc8a3a
mateuszb5168 <mateusz_biela@wp.pl>
2026-01-21 15:59:01 +0100

    finished the chapter json, basics

--- javaScript/JS18_JSON/main.js
+++ javaScript/JS18_JSON/main.js
@@ -10,3 +10,12 @@ console.log(myObj);
 console.log(myObj.name);
 myObj.hello();
 console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+console.log(sendJSON);// no hello function, json doesnt send functions 
+console.log(typeof sendJSON);
+console.log(sendJSON.name); //undefined, the variable is a string not object 
+const recieveJSON = JSON.parse(sendJSON);//convert to object
+console.log(recieveJSON);
+
+//i guess these were only the basics, alright
